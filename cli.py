5846dab218c34e91a8824b0b1e75e333a24b7ed6
Sreeja <sreejaperumandla06gmial.com>
2026-06-01 10:00:48 +0530

    Initial commit

---
+++ cli.py
@@ -0,0 +1,13 @@
+class Task:
+    title=""
+    completed="False"
+    def __init__(self,name):
+        self.title=name
+        #self.completed="False"
+    def to_dict():
+    def from_dict():
+    
+class TaskManager:
+    tasks=[]
+    filename = "tasks.json"
+    def add_task():

6b2057b74b331920d5f7f86c8a04c656b6383baa
Sreeja <sreejaperumandla06gmial.com>
2026-06-01 10:28:21 +0530

    Added mark task complete feature

--- cli.py
+++ cli.py
@@ -1,22 +1,25 @@
 class Task:
     def __init__(self,name):
         self.title=name
-        self.completed="False"
+        self.completed=False
     def to_dict():
     def from_dict():
     
 class TaskManager:
     def __init__(self):
         self.tasks = []
-    def add_task(title):
+    def add_task(self,title):
         task=Task(title)
         self.tasks.append(task)
     def list_tasks(self):
-        if task not in tasks:
+        if task not in self.tasks:
             print("No tasks found")
             return False
         for i,task in enumerate(self.tasks, start=1):
             status = "✓" if task.completed else " "
             print(f"{i}. {task.title} [{status}]")
+    def mark_done(self,ind):
+        self.tasks[ind].completed=True
+
     def del_task():
     def save_task():    
